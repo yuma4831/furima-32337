b658b77e909632a2c54c6701b4fe08dfecbfb55a
yuma <yuma.4831@gmail.com>
2021-04-11 15:45:03 +0900

    クウォーテーションの統一

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "全角ひらがな、全角カタカナ、漢字で入力して下さい" } do
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: '全角ひらがな、全角カタカナ、漢字で入力して下さい' } do
       validates :lastname
       validates :firstname
     end
-    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力して下さい" } do
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力して下さい' } do
       validates :lastname_kana
       validates :firstname_kana
     end
