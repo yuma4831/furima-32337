22606df7394d3755c4241288034f5c5ac8203603
yuma <yuma.4831@gmail.com>
2021-05-01 13:38:51 +0900

    価格手数料、利益の算出実装

--- app/javascript/packs/item-price.js
+++ app/javascript/packs/item-price.js
@@ -3,6 +3,12 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+    const fee = inputValue*0.1
+    addTaxDom.innerHTML = Math.floor(fee)
+
+    const addProfitDom = document.getElementById("profit");
+    const profit = inputValue-fee
+    addProfitDom.innerHTML = Math.floor(profit)
   })
 });
