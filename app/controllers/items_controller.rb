349e5d663eae4e49c5af04b0120c3999a492e16d
yuma <yuma.4831@gmail.com>
2021-05-08 18:29:42 +0900

    URLから移動できないようにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
   def index
     @items = Item.includes(:user).order(created_at: :desc)
   end
@@ -41,6 +42,10 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @item.user
   end
 
 end
