fec9618e749b74499001391bc7ec375fc6559971
yuma <yuma.4831@gmail.com>
2021-05-09 13:58:12 +0900

    フロント実装

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,2 +1,22 @@
 class RecordsController < ApplicationController
+
+  def index
+    @records = Order.new
+  end
+
+  def create
+    @record = Order.new(record_params)
+    if @record.valid?
+      @record.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def record_params
+    params.require(:record).permit()
+  end
 end
